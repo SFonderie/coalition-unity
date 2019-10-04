a49275142c4c23ff10e1658b73a1a4215bfaacf8
DanCode68 <danbob68@gmail.com>
2019-10-03 20:37:23 -0700

    adjust variable values

--- Assets/SampleFloorGen.cs
+++ Assets/SampleFloorGen.cs
@@ -30,11 +30,11 @@ public class SampleFloorGen : MonoBehaviour
                 sr = go.AddComponent<SpriteRenderer>();
                 sr.sprite = primary;
                 go.transform.Translate(j, i, 0.5f);
-                go.transform.localScale = new Vector3(1.65f, 1.65f, 1f);
+                go.transform.localScale = new Vector3(1.55f, 1.55f, 1f);
             }
         }
 
-        for (float i = -1 * (height / 2) - 0.25f; i < (height / 2) + 0.5f; i += 0.25f)
+        for (float i = -1 * (height / 2) - 0.25f; i < (height / 2) + 0.5f; i += 0.5f)
         {
             for (float j = -1 * (width / 2) + 0.5f; j <= (width / 2) - 0.5f; ++j)
             {
@@ -42,7 +42,7 @@ public class SampleFloorGen : MonoBehaviour
                 sr = go.AddComponent<SpriteRenderer>();
                 sr.sprite = secondary;
                 go.transform.Translate(j, i, 0.5f);
-                go.transform.localScale = new Vector3(1.65f, 1.65f, 1f);
+                go.transform.localScale = new Vector3(1.55f, 1.55f, 1f);
             }
         }
     }
