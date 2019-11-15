39e4e2ab2a674b9930aca38953d6400665acf000
GlenvillePecor <glenvillepecor2@gmail.com>
2019-11-14 19:23:05 -0800

    https://www.youtube.com/watch?v=lamO7MIEOU0&feature=youtu.be
    https://www.youtube.com/watch?v=lamO7MIEOU0&feature=youtu.be

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speedWP;
+    public float waitTime;
+    public float sWaitTime;
+    public Transform[] moveSpots;
+    private int i = 0;
+    private int r = 0;
+    private Controller m_Controller; 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        m_Controller = GetComponent<Controller>();
+        waitTime = sWaitTime = ((m_Controller.turnFloat * 2 )) / m_Controller.m_Speed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (r == 0){
+            //change this to make it turned base, use maybe for first if statment
+            
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].position, speedWP * Time.deltaTime);
+            if (Vector2.Distance(transform.position, moveSpots[i].position) < 0.2f)
+            {
+                
+                //m_Controller.m_Speed = 0 ;
+                if (waitTime <= 0) //do && is turn
+                {
+                    if (i < moveSpots.Length - 1)
+                    {
+                        Debug.Log("Next");
+                        
+                        i++;
+
+                    }
+                    else
+                    {
+                        Debug.Log("Reset");
+                        i = 0;
+                    }
+
+                                       
+
+                    waitTime =  (m_Controller.turnFloat * 2) / m_Controller.m_Speed; 
+
+
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+
+            }
+        }
+    }
+}
